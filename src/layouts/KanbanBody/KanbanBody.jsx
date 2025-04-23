0a31c03ed499a311138302b60668a8c3d8a5b3c1
helenaemptiness <lenameowquack@gmail.com>
2025-04-23 20:22:40 +0600

    added layout components KanbanBody and LeftPanel

---
+++ src/layouts/KanbanBody/KanbanBody.jsx
@@ -0,0 +1,9 @@
+import styles from './KanbanBody.module.css';
+
+function KanbanBody() {
+	return (
+		<div className={styles.kanban__body}></div>
+	);
+}
+
+export default KanbanBody
---
+++ src/layouts/LeftPanel/LeftPanel.jsx
@@ -0,0 +1,9 @@
+import styles from './LeftPanel.module.css';
+
+function LeftPanel() {
+	return (
+		<div className={styles.left__panel}></div>
+	);
+}
+
+export default LeftPanel
